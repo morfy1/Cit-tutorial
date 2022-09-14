5f7479003debbc01d09e8acdd4d6b83f847b9fed
Makka <makka@Makkas-MacBook-Air.local>
2022-09-14 15:57:36 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class ProfileClass extends React.Component {
+  render(props) {
+    const days = new Date(this.props.registredAt).getDate();
+    const mesyacs = new Date().getMonth() + 1;
+    const years = new Date(this.props.registredAt).getFullYear();
+
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Привет, <b>{this.props.name}!</b>{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          Дата регистрации: {days} {montoToStr(mesyacs)} {years}
+        </p>
+      </div>
+    );
+  }
+}
+
+function Profile(props) {
+  const day = new Date(props.registredAt).getDate();
+  const mesyac = new Date().getMonth() + 1;
+  const year = new Date(props.registredAt).getFullYear();
+
+  return (
+    <div>
+      <p>
+        Привет, <b>{props.name}!</b>{" "}
+      </p>
+      <p>
+        Дата регистрации:{day} {montoToStr(mesyac)} {year}
+      </p>
     </div>
   );
+}
+function App() {
+  return (
+    <div>
+      <ProfileClass name="Вася" registredAt={new Date()} />
+      <Profile name="Вася" registredAt={new Date()} />
+    </div>
+  );
+}
+function montoToStr(num) {
+  return num > 12 || num < 1
+    ? null
+    : "январь,февраль,март,апрель,май,июнь,июль,август,сентябрь,октябрь,ноябрь,декабрь".split(
+        ","
+      )[num - 1];
 }
 
 export default App;
---
+++ src/makka.js
@@ -0,0 +1,4 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "makka.js";
+import { render } from "@testing-library/react";
